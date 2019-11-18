aba2fa97afbf6917b922bf6683a3a901ec49e512
lordChipotle <36554153+lordChipotle@users.noreply.github.com>
2019-11-18 16:58:05 +0000

    Update LineFollower.java

--- LineFollower.java
+++ LineFollower.java
@@ -1,352 +1,102 @@
-
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.motor.UnregulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.*;
 
 public class LineFollower {
-
-
-            /** The EV3 brick we're controlling */
-
-       private static EV3 brick;
-
-
-       /** The motor on the left side of the robot */
-
-       private static RegulatedMotor motorB;
-
-
-       /** The motor on the right side of the robot */
-
-       private static RegulatedMotor motorC;
-
-
-       /** The raw EV3 Color Sensor object */
-
-       private static EV3ColorSensor colorSensor;
-
-       
-
-
-       /** The raw EV3 Ultrasonic Sensor object */
-
-       private static EV3UltrasonicSensor ultrasonicSensor;
-
-
-       private static SampleProvider calibrated; // use this to fetch samples
-
-       
-
-       public static void main(String[] args) {
-
-               motorB = new UnregulatedMotor(SerialPort.);
-
-               motorC = new UnregulatedMotor(SerialPort.);
-
-               colorSensor = new EV3ColorSensor(SerialPort.);
-
-               ultrasonicSensor = new EV3UltrasonicSenaor(SerialPort.)
-
-               move(); // start it running
-
-       }
-
-       private static void go() {
-
-               // only calibrate once, at the start
-
-               calibrate();
-
-
-               // so long as we can keep finding the line
-
-               while (findLine()) {
-
-                       // go forward until the line is lost
-
-                       followLine();
-
-               }
-
-       }
-
-
-       private void calibrate() {
-
-               // clear the screen
-
-               brick.getTextLCD().clear();
-
-
-               brick.getTextLCD().drawString("Calibrate:", 0, 0);
-
-               brick.getTextLCD().drawString("Place the robot", 0, 1);
-
-               brick.getTextLCD().drawString("on or near the", 0, 2);
-
-               brick.getTextLCD().drawString("line start.", 0, 3);
-
-               brick.getTextLCD().drawString("Then press the", 0, 5);
-
-               brick.getTextLCD().drawString("Enter key", 0, 6);
-
-
-               brick.getKey("Enter").waitForPressAndRelease();
-
-
-               // put the sensor in "red" mode to shine a light and
-
-               // read back a brightness value
-
-               //
-
-               // The two numbers are the min and max scaled values
-
-               // you want to receive (you can make them whatever you want
-
-               // but the default is to return numbers between 0 and 1).
-
-               //
-
-               // The boolean determines if the filter "clamps" values out
-
-               // of range to the minimum and maximum provided.  If set to
-
-               // true, you will always get values in the range you specify.
-
-               // If false, a poor calibration may result in readings coming
-
-               // back outside of range.
-
-               NormalizationFilter nf = new NormalizationFilter
-
-                               (colorSensor.getRedMode(), 0, 1, true);
-
-
-               calibrated = nf;
-
-
-               // signal the filter to start the calibration process
-
-               nf.startCalibration();
-
-
-               findInitialLine();
-
-
-               // end the collection of calibration data
-
-               nf.stopCalibration();
-
-
-               brick.getTextLCD().clear();
-
-       }
-
-
-       private static void turn(int degrees) {
-
-               motorB.turn(degrees);
-
-               motorC.turn(degrees);
-
-       }
-
-       
-
-       private static void findInitialLine() {
-
-               // You will need to complete this method in order to
-
-               // calibrate the sensor and get your robot in the correct
-
-               // starting position
-
-
-               // Use the "calibrated" variable as your sample source
-
-               // to call fetchSample() on
-
-
-               // Turn using turn(), and take samples while its turning
-
-
-               // When you've finished turning, turn back to the location
-
-               // where you saw the best reading.
-
-               //
-
-               // Hint: see the following methods that your motors support:
-
-               // http://web.suffieldacademy.org/cs/lejos_ev3_api/lejos/robotics/Encoder.html#getTachoCount--
-
-               // http://web.suffieldacademy.org/cs/lejos_ev3_api/lejos/robotics/Encoder.html#resetTachoCount--
-
-       }
-
-
-       private static void followLine(){
-
-           double Kp = …;
-
-
-	
-
- double Ki = …; 
-
-
-	
-
- double Kd = …; 
-
-
-	
-
- int error = 0;
-
-
-	
-
- int integral = 0;
-
-
-	
-
- int derivative = 0;
-
-
-	
-
-int lastError = 0;
-
-
-	
-
-double correction = 0;
-
-
-
-	
-
-int color;
-
-
-	
-
-double cTurn;
-
-
-	
-
-double bTurn;
-
-//threshold = (LFPIDController.lvh.getBlack() + LFPIDController.lvh
-
-
-	
-
-.getWhite()) / 2;
-
-
-while(colorSensor.getColorID() == 1){
-
-color = colorSensor.getFloodLight(); / getAmbientMode(); 
-
-     error = color - threshold;
-
-
-	
-
-integral = error + integral;
-
-
-	
-
-derivative = error - lastError;
-
-
-	
-
-
-
-	
-
-correction = kp * error + ki * integral + kd * derivative;
-
-
-	
-
-
-
-	
-
-bTurn = 20 - correction;
-
-
-	
-
-cTurn = 20 + correction;
-
-mB.setPower(new Double(bTurn).intValue());
-
-
-	
-
-mB.forward();
-
-
-	
-
-mC.setPower(new Double(cTurn).intValue());
-
-
-	
-
-mC.forward();
-
-
-	
-
-
-
-	
-
-lastError = error;
-
-
+/** The EV3 brick we're controlling */
+private static EV3 brick;
+
+/** The motor on the left side of the robot */
+private static UnregulatedMotor motorB;
+
+/** The motor on the right side of the robot */
+private static UnregulatedMotor motorC;
+
+/** The raw EV3 Color Sensor object */
+private static EV3ColorSensor colorSensor;
+	        
+/** The raw EV3 Ultrasonic Sensor object */
+private static EV3UltrasonicSensor ultrasonicSensor;
+
+private static SampleProvider calibrated; // use this to fetch samples
+	        
+public static void main(String[] args) {
+ motorB = new UnregulatedMotor(MotorPort.B);
+ motorC = new UnregulatedMotor(MotorPort.C);
+ colorSensor = new EV3ColorSensor(SensorPort.S3);
+ ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
+ move(); // start it running
+}
+private static void move() {
+// only calibrate once, at the start
+calibrate();
+
+// so long as we can keep finding the line...
+while (findLine()) { followLine(); }
+}
+
+private static void calibrate() {
 
 }
 
+private void turn(float degrees) {
+...
+}
+	        
+private void findInitialLine() {
 
-       }
+	                // Use the "calibrated" variable as your sample source
+	                // to call fetchSample() on
 
+	                // Turn using turn(), and take samples while its turning
 
-
-
-       private static boolean findLine() {
-
-               float[] sample = new float[calibrated.sampleSize()];
-
-               sample[0] = -1;
-
-               while (sample[0] > 0.5) {
-
-                   calibrated.fetchSample(sample, 0);
-
-               }
-
-               int i = 0; 
-
-while(colorSensor.getColorID() != 1) {
-
-turn(sample[i]); i++; 
-
-                }
-
-
-       }
-
-
-
+	                // When you've finished turning, turn back to the location
+	                // where you saw the best reading.
+	                //
+	                // Hint: see the following methods that your motors support:
+	                // http://web.suffieldacademy.org/cs/lejos_ev3_api/lejos/robotics/Encoder.html#getTachoCount--
+	                // http://web.suffieldacademy.org/cs/lejos_ev3_api/lejos/robotics/Encoder.html#resetTachoCount--
 }
 
+private static void followLine(){
+ double Kp = 500;
+ double Ki = 375; 
+ double Kd = 25; 
+ double integral = 0;
+ double derivative = 0;
+ double lastError = 0;
+ double dt = ...;
+ //double correction = 0; 
+ double cTurn;
+ double bTurn;
+ double offset = 45; 
+ double Tp = 50; 
+ while(colorSensor.getColorID() == 1){
+  double lightValue = colorSensor.getFloodlight();
+  double error = lightValue - offset; 
+  integral = integral + error * dt; 
+  derivative = (error - lastError)/dt; 
+  double turn = Kp * offset + Ki * integral + Kd * derivative;
+  bTurn = Tp + turn; 
+  cTurn = Tp - turn;
+  lastError = error; 
+  motorB.setPower(new Double(bTurn).intValue());
+  motorB.forward();
+  motorC.setPower(new Double(cTurn).intValue());
+  motorC.forward();
+  }
+}
 
+private static boolean findLine() {
+	float[] sample = new float[calibrated.sampleSize()];
+	sample[0] = -1;
+	while (sample[0] > 0.5) { calibrated.fetchSample(sample, 0);}
+	int i = 0; 
+	while(colorSensor.getColorID() != 1) { turn(sample[i]); i++; }
+	return true;
 }
