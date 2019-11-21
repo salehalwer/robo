bf0d48f398975a3b8d91a057e2b17dc580781f42
lordChipotle <36554153+lordChipotle@users.noreply.github.com>
2019-11-21 17:52:59 +0000

    Update LineFollower.java

--- LineFollower.java
+++ LineFollower.java
@@ -1,52 +1,133 @@
 import lejos.hardware.ev3.EV3;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.*;
-
+import java.lang.Math; 
 public class LineFollower {
 /** The EV3 brick we're controlling */
-private static EV3 brick;
+private  EV3 brick;
+
+private float lastAngle; 
 
 /** The motor on the left side of the robot */
-private static UnregulatedMotor motorB;
+private  UnregulatedMotor motorB;
 
 /** The motor on the right side of the robot */
-private static UnregulatedMotor motorC;
+private  UnregulatedMotor motorC;
 
 /** The raw EV3 Color Sensor object */
-private static EV3ColorSensor colorSensor;
-	        
-/** The raw EV3 Ultrasonic Sensor object */
-private static EV3UltrasonicSensor ultrasonicSensor;
+private  EV3ColorSensor colorSensor;
 
-private static SampleProvider calibrated; // use this to fetch samples
+private  EV3GyroSensor gyro;
+/** The raw EV3 Ultrasonic Sensor object */
+private  EV3UltrasonicSensor ultrasonicSensor;
+
+private SampleProvider gyroProvider; // use this to fetch samples
+private SampleProvider colorProvider; 
+private SampleProvider distanceProvider; 
 	        
-public static void main(String[] args) {
+public  void main(String[] args) {
  motorB = new UnregulatedMotor(MotorPort.B);
  motorC = new UnregulatedMotor(MotorPort.C);
  colorSensor = new EV3ColorSensor(SensorPort.S3);
  ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
+ gyro = new EV3GyroSensor(SensorPort.S2);
+ gyroProvider = gyro.getAngleMode();
+ colorProvider = colorSensor.getMode("color");
+ distanceProvider = ultrasonicSensor.getMode("distance");
+ gyro.reset();
  move(); // start it running
 }
-private static void move() {
-// only calibrate once, at the start
-calibrate();
-
+private  void move() {
+	while(colorSensor.getColorID() == 5) {
+		motorB.stop();
+		motorC.stop();
+	}		
 // so long as we can keep finding the line...
 while (findLine()) { followLine(); }
 }
 
-private static void calibrate() {
+public void tankDrive(double left, double right) {
+	if(left>100) {
+		left = 100;
+	}
+	else if (left<-100) {
+		left = -100;
+	}
+	if(right>100) {
+		right =100;
+	}
+	else if(right<-100) {
+		right = -100;
+	}
+	if (left>0) {
+		motorB.setPower( (int) Math.abs(right));
 
+		motorB.forward();
+		if (right>0) {
+			motorC.setPower( (int) Math.abs(right));
+			motorC.forward();
+		}
+		else if (right<0) {
+			motorC.setPower( (int) Math.abs(right));
+
+			motorC.backward();
+		}
+		else {
+			motorC.stop();
+		}
+	}
+	else if (left<0) {
+		motorB.setPower( (int) Math.abs(right));
+
+		motorB.backward();
+	}
+	else {
+		motorB.stop();
+	}
+		
+	}
+
+public void arcadeDrive(double throttleValue, double turnValue) {
+    double leftMtr;
+    double rightMtr;
+    leftMtr = throttleValue + turnValue;
+    rightMtr = throttleValue - turnValue;
+    tankDrive(leftMtr, rightMtr);
 }
-
+public float getM() {
+	float[] sample = new float[gyroProvider.sampleSize()];
+	while(true) 
+		  return sample[0];
+}
 private void turn(float degrees) {
-...
+	int P, I, D = 1;
+    int integral, previous_error, setpoint = 0;
+    double error = setpoint - getM();
+	
+	while (getM()!=degrees) {
+		
+		if (degrees>0) {
+			motorB.backward();
+			motorC.forward();
+
+		}
+		else if(degrees<0) {
+			motorB.forward();
+			motorC.backward();
+		}
+		else {
+			arcadeDrive(0,0);
+		}
+	}
 }
+	
 	        
 private void findInitialLine() {
 
@@ -63,22 +144,23 @@ private void findInitialLine() {
 	                // http://web.suffieldacademy.org/cs/lejos_ev3_api/lejos/robotics/Encoder.html#resetTachoCount--
 }
 
-private static void followLine(){
- double Kp = 500;
+private  void followLine(){
+ double Kp = 0.005;
  double Ki = 375; 
  double Kd = 25; 
  double integral = 0;
  double derivative = 0;
  double lastError = 0;
- double dt = ...;
- //double correction = 0; 
+ double dt = 0;
  double cTurn;
  double bTurn;
  double offset = 45; 
  double Tp = 50; 
  while(colorSensor.getColorID() == 1){
+  lastAngle = ...;
   double lightValue = colorSensor.getFloodlight();
   double error = lightValue - offset; 
+  dt = 0;
   integral = integral + error * dt; 
   derivative = (error - lastError)/dt; 
   double turn = Kp * offset + Ki * integral + Kd * derivative;
@@ -92,11 +174,10 @@ private static void followLine(){
   }
 }
 
-private static boolean findLine() {
-	float[] sample = new float[calibrated.sampleSize()];
+private  boolean findLine() {
+	float[] sample = new float[colorProvider.sampleSize()];
 	sample[0] = -1;
-	while (sample[0] > 0.5) { calibrated.fetchSample(sample, 0);}
-	int i = 0; 
-	while(colorSensor.getColorID() != 1) { turn(sample[i]); i++; }
+	while (sample[0] != 1) { turn(...towards lastAngle); colorProvider.fetchSample(sample, 0);}
 	return true;
+}
 }
